c898784f2fa8e0e8a8f8b38ee44ed7b4a7d738f4
Kurucz Tímea <79456559+ktimi30@users.noreply.github.com>
2021-06-05 19:03:00 +0200

    Add files via upload

---
+++ js/szallasfoglalas.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", init, false);
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function init() {
+    $("btn").addEventListener("click", foglalas, false);
+}
+
+function foglalas() {
+    ures($("nev"));
+    ures($("email"));
+    ures($("szam"));
+    ures($("erkezes"));
+    ures($("tavozas"));
+    ures($("felnottek"));
+    ures($("gyerekek"));
+    ures($("szoba"));
+    if ($("szam") >= 10) {
+        window.alert("Hibás telefonszámot adott meg!");
+    }
+    if ($("felnottek").value < 0) {
+        window.alert("A felnőttek száma hibás!");
+    }
+    if ($("gyerekek").value < 0) {
+        window.alert("A felnőttek száma hibás!");
+    }
+    if ($("szoba") > 8 || $("szoba" < 8)) {
+        window.alert("Hibás kódot adott meg!");
+    }
+    $("foglalas").innerHTML = "Kedves " + $("nev").value + "! A szobafoglalás sikeresen megtörtént!";
+}
+
+function ures(ertek) {
+    if (ertek.value.length == 0) {
+        window.alert("A mezők kitöltése kötelező!");
+        $("form").reset();
+        $("btn").removeEventListener();
+    }
+}
